2f8510c84a4d6600911e9f7a36ea2b0b39713a38
James Maniscalco <maniscalco.jt@gmail.com>
2014-01-29 22:51:59 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gearlocker::Application.config.secret_key_base = 'ca6d485da44ba092de0992209acd08c62fc22a1cc1a374312154fbbe8c776409f487e0fe4dda307049d8f82d5fc7f8dd1ff78fba34634d371f58c61d1b41acf1'
